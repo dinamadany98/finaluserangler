05cdbae2b5f3dfcb79a9e8e7227b303c8e8c4501
Dina Madany <dodyymadany98@gmail.com>
2022-04-12 20:14:45 +0200

    register

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: true,
-  APIBaseURL: 'http://localhost:8000/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  APIBaseURL: 'http://localhost:8000/api'
+ 
 };
 
 /*
