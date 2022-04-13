605f0cd2052c826d3b6a217a35012a0481f4672d
Dina Madany <dodyymadany98@gmail.com>
2022-04-13 18:07:05 +0200

    cart component

--- src/app/Components/cart/cart.component.ts
+++ src/app/Components/cart/cart.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Icart } from 'src/app/Models/icart';
+import { Icheckout } from 'src/app/Models/icheckout';
+import { IProduct } from 'src/app/Models/iproduct';
+import { CartService } from 'src/app/Services/cart.service';
+import { CheckoutService } from 'src/app/Services/checkout.service';
+import { IProductService } from 'src/app/Services/iproduct.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  newshipping:Icheckout={} as Icheckout
+  cartlist:Icart[]=[];
+  prdlisticat: IProduct[] = [];
+  constructor(private rot:Router,private checkservice:CheckoutService,private cartservice:CartService,
+    private prdapisevice: IProductService) { }
 
   ngOnInit(): void {
+    this.prdapisevice.getallproduct().subscribe((prdlist) => {
+      this.prdlisticat = prdlist;
+    }); 
+
+
+    this.cartservice.getcartdata().subscribe(cart=>{
+      this.cartlist=cart;
+    }); 
+  
   }
 
+  total=0
+  sum(pric:any,count:any){
+   this.total+=(+pric * +count);
+return +pric * +count;
+  }
 }
