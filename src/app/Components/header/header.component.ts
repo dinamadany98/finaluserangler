bbb432998b93827c3760bd7fd8bc8b2ba1bef06f
Shaymaa Mohamed Gaafar <shaymaagaafar11@gmail.com>
2022-04-17 00:55:44 +0200

    well

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
+import { IProduct } from 'src/app/Models/iproduct';
+import { IProductService } from 'src/app/Services/iproduct.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  filterBy: any;
+  product: IProduct[] = [];
+  constructor(private prodservic: IProductService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  filter() {
+    this.prodservic.searchproduct(this.filterBy).subscribe((prod) => {
+      this.product = prod;
+    });
   }
-
 }
