5b828bcc16840d0e7e2aba635a8df701813e8b05
Shaymaa Mohamed Gaafar <shaymaagaafar11@gmail.com>
2022-04-17 06:25:55 +0200

    new home

--- src/app/Components/search/search.component.ts
+++ src/app/Components/search/search.component.ts
@@ -1,23 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { IProduct } from 'src/app/Models/iproduct';
 import { IProductService } from 'src/app/Services/iproduct.service';
+import { WhishlistService } from 'src/app/Services/whishlist.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-  filterBy:any
-  product:IProduct[]=[]
-  constructor(private prodservic:IProductService) { }
+  filterBy: any;
+  product: IProduct[] = [];
+  constructor(
+    private prodservic: IProductService,
+    private rot: Router,
+    private whishlistservic: WhishlistService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   filter() {
-
-    this.prodservic.searchproduct(this.filterBy).subscribe(prod=>{
-      this.product=prod;
+    this.prodservic.searchproduct(this.filterBy).subscribe((prod) => {
+      this.product = prod;
     });
-      }
+  }
+  ///////////////////////////////
+  addtowishlist(prod: IProduct) {
+    this.whishlistservic.addwishlistdata(prod).subscribe({
+      next: (prod) => {
+        this.rot.navigate(['/']);
+      },
+
+      error: (err) => {
+        alert('error');
+      },
+    });
+  }
 }
