71ff3ab3d5e240f5e5aa08b62ac8f572ea6162a9
Shaymaa11 <94070574+Shaymaa11@users.noreply.github.com>
2022-04-17 15:13:03 +0200

    Update header.component.ts

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/Services/login.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private loginservice: LoginService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  loggedin() {
+    return !!localStorage.getItem('login');
   }
-
+  logoutuser() {
+    localStorage.removeItem('login');
+    this.router.navigate(['/home']);
+  }
+  ngOnInit(): void {}
 }
